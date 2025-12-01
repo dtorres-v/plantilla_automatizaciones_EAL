810b7a15d52a3c162b906c6b84cdf61872b9791f
Dominique Torres Viguri <dtorres@powerstreet.com.mx>
2025-12-01 13:16:43 -0600

    first commit

--- EAL/VCO_TC001.py
+++ EAL/VCO_TC001.py
@@ -1,4 +1,5 @@
 import pytest
+from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
@@ -11,7 +12,7 @@ class TestRecorder:
     # ---------------------------------------------------------
     # HELPER: Función WAIT
     # ---------------------------------------------------------
-    def wait_for_element(self, by, value, timeout=20):
+    def wait_for_element(self, by, value, timeout=100):
         """
         Helper para esperar explícitamente a un elemento.
         Usa self.driver que viene inyectado desde el conftest.
@@ -35,6 +36,32 @@ class TestRecorder:
         # Asegúrate de sí usar la función self.wait_for_element(...) cuando sea requerida
         # el1 = self.wait_for_element(by=AppiumBy.CLASS_NAME, value="android.widget.Button")
         # el1.click()
+        """ingreso"""
+        el1 = driver.find_element(by=AppiumBy.CLASS_NAME, value="android.widget.Button")
+        el1.click()
+        el2 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Usuario")
+        el2.click()
+        el2.send_keys("anevvv001")
+        el3 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Contraseña")
+        el3.click()
+        el3.send_keys("Pwst12345*")
+        el4 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Servidor")
+        el4.click()
+        el4.clear()
+        el4.send_keys("dsr-mobile.pwstasp.com.uy/mobileservices")
+        driver.execute_script('mobile:pressKey', {"keycode": 4})
+        el5 = driver.find_element(by=AppiumBy.CLASS_NAME, value="android.widget.CheckBox")
+        el5.click()
+        el6 = driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"Instalar\")")
+        el6.click()
+        """carga"""
+        el7 = self.wait_for_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"cargar\")")
+        el7.click()
+        """"aceptar permisos"""
+        el8 = self.wait_for_element(by=AppiumBy.ID, value="com.android.packageinstaller:id/permission_allow_button")
+        el8.click()
+        el9 = driver.find_element(by=AppiumBy.ID, value="android:id/button2")
+        el9.click()
 
 
 
